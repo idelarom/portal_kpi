fbca4d0c23f9195f9f62eb909364149075193b3d
RCASTILA <RCASTILA@MTL-RCASTILA10>
2017-06-21 18:22:49 -0500

    21/06/2017 06:22 p.m.

--- presentacion/login.aspx.cs
+++ presentacion/login.aspx.cs
@@ -111,7 +111,7 @@ namespace presentacion
 
         protected void lnkcambiardominio_Click(object sender, EventArgs e)
         {
-            //
+            //jajajajajaja
             div_dominio.Visible = div_dominio.Visible ? false : true;
         }
     }
