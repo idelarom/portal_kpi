7c8a1855b8e1e1793f62d55ce050699db10192c0
Roberto Armando Castillo Albino <roberto_castillo@migesa.com.mx>
2017-06-26 18:23:11 -0500

    ROBERT 26/06/2017 06:22 P.M

---
+++ negocio/Componentes/WidgetsCOM.cs
@@ -0,0 +1,118 @@
+﻿using datos.NAVISION;
+using datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+
+
+namespace negocio.Componentes
+{
+    public class WidgetsCOM
+    {
+        public DataSet sp_editar_widgets(int id_perfil, string perfil, string usuario, string cadena_usuarios, int total_cadena_usuarios, string cadena_widgets, int total_cadena_widgets)
+        {
+            DataSet ds = new DataSet();
+            List<SqlParameter> listparameters = new List<SqlParameter>();
+            Datos data = new Datos();
+            listparameters.Add(new SqlParameter() { ParameterName = "@pid_perfil", SqlDbType = SqlDbType.Int, Value = id_perfil });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pperfil", SqlDbType = SqlDbType.Int, Value = perfil });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pusuario", SqlDbType = SqlDbType.Int, Value = usuario });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pcadena_usuarios", SqlDbType = SqlDbType.Int, Value = cadena_usuarios });
+            listparameters.Add(new SqlParameter() { ParameterName = "@ptotal_cadena_usuarios", SqlDbType = SqlDbType.Int, Value = total_cadena_usuarios });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pcadena_widgets", SqlDbType = SqlDbType.Int, Value = cadena_widgets });
+            listparameters.Add(new SqlParameter() { ParameterName = "@ptotal_cadena_widgets", SqlDbType = SqlDbType.Int, Value = total_cadena_widgets });
+            try
+            {
+                //ds = data.datos_Clientes(listparameters);
+                ds = data.enviar("sp_editar_perfiles", listparameters, false, 1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ds;
+        }
+
+        public DataSet sp_agregar_widgets(string perfil, string usuario, string cadena_usuarios, int total_cadena_usuarios, string cadena_widgets, int total_cadena_widgets)
+        {
+            DataSet ds = new DataSet();
+            List<SqlParameter> listparameters = new List<SqlParameter>();
+            Datos data = new Datos();
+            listparameters.Add(new SqlParameter() { ParameterName = "@pperfil", SqlDbType = SqlDbType.Int, Value = perfil });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pusuario", SqlDbType = SqlDbType.Int, Value = usuario });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pcadena_usuarios", SqlDbType = SqlDbType.Int, Value = cadena_usuarios });
+            listparameters.Add(new SqlParameter() { ParameterName = "@ptotal_cadena_usuarios", SqlDbType = SqlDbType.Int, Value = total_cadena_usuarios });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pcadena_widgets", SqlDbType = SqlDbType.Int, Value = cadena_widgets });
+            listparameters.Add(new SqlParameter() { ParameterName = "@ptotal_cadena_widgets", SqlDbType = SqlDbType.Int, Value = total_cadena_widgets });
+            try
+            {
+                //ds = data.datos_Clientes(listparameters);
+                ds = data.enviar("sp_agregar_perfiles", listparameters, false, 1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ds;
+        }
+
+        public DataSet sp_borrar_widgets(int pid_perfil, string usuario, string comentarios)
+        {
+            DataSet ds = new DataSet();
+            List<SqlParameter> listparameters = new List<SqlParameter>();
+            Datos data = new Datos();
+            listparameters.Add(new SqlParameter() { ParameterName = "@pid_perfil", SqlDbType = SqlDbType.Int, Value = pid_perfil });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pusuario", SqlDbType = SqlDbType.Int, Value = usuario });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pcomentarios", SqlDbType = SqlDbType.Int, Value = comentarios });
+            try
+            {
+                //ds = data.datos_Clientes(listparameters);
+                ds = data.enviar("sp_borrar_perfiles", listparameters, false, 1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ds;
+        }
+
+        public DataSet sp_catalogo_widgets(int pid_perfil)
+        {
+            DataSet ds = new DataSet();
+            List<SqlParameter> listparameters = new List<SqlParameter>();
+            Datos data = new Datos();
+            listparameters.Add(new SqlParameter() { ParameterName = "@pid_widget", SqlDbType = SqlDbType.Int, Value = pid_perfil });
+            try
+            {
+                //ds = data.datos_Clientes(listparameters);
+                ds = data.enviar("sp_catalogo_widgets", listparameters, false, 1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ds;
+        }
+
+        public DataSet sp_usuarios_widgets(int pid_perfil)
+        {
+            DataSet ds = new DataSet();
+            List<SqlParameter> listparameters = new List<SqlParameter>();
+            Datos data = new Datos();
+            listparameters.Add(new SqlParameter() { ParameterName = "@pid_perfil", SqlDbType = SqlDbType.Int, Value = pid_perfil });
+            try
+            {
+                //ds = data.datos_Clientes(listparameters);
+                ds = data.enviar("sp_usuarios_perfiles", listparameters, false, 1);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ds;
+        }
+    }
+}
---
+++ presentacion/catalogo_widgets.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using negocio.Componentes;
+using System.Data;
+
+namespace presentacion
+{
+    public partial class catalogo_widgets : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Cargar_catalogo_widgets(0);
+            }
+        }
+        private void Cargar_catalogo_widgets(int id_widget)
+        {
+            try
+            {
+                WidgetsCOM widcom = new WidgetsCOM();
+                DataSet ds = widcom.sp_catalogo_widgets(id_widget);
+                grid_widgets.DataSource = ds.Tables[0];
+                grid_widgets.DataBind();
+            }
+            catch (Exception ex)
+            {
+
+                Toast.Error("Error al cargar el catalogo de widgets: " + ex.Message,this);
+            }
+        }
+    }
+}
