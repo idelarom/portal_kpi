e8258c8c6831f9b39a360e1a2e13f516fea23675
Roberto Armando Castillo Albino <roberto_castillo@migesa.com.mx>
2017-08-25 10:10:17 -0500

    Robert 25/08/2017 10:10 am

---
+++ negocio/Componentes/DashboardCompromisosCOM.cs
@@ -0,0 +1,35 @@
+﻿using datos.NAVISION;
+using datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+
+
+namespace negocio.Componentes
+{
+    public class DashboardCompromisosCOM
+    {
+        public DataSet spq_Ingenieros_Performance(DateTime? fecha_ini, DateTime? fecha_fin, string pLstEmpleados, string Usr, int Tipo)
+        {
+            DataSet ds = new DataSet();
+            List<SqlParameter> listparameters = new List<SqlParameter>();
+            Datos data = new Datos();
+            listparameters.Add(new SqlParameter() { ParameterName = "@pFechaInicial", SqlDbType = SqlDbType.Int, Value = fecha_ini });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pFechaFinal", SqlDbType = SqlDbType.Int, Value = fecha_fin });
+            listparameters.Add(new SqlParameter() { ParameterName = "@pLstEmpleados", SqlDbType = SqlDbType.Int, Value = pLstEmpleados });
+            try
+            {
+                //ds = data.datos_Clientes(listparameters);
+                ds = data.enviar("Sps_DashBoardCompromisos", listparameters, false, 3);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ds;
+        }
+    }
+}
