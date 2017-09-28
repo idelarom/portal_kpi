3305c8117f0d4a509fe9c2152013c04522b324ee
Roberto <isaaac_delarosa@migesa.com.mx>
2017-09-28 15:49:52 -0500

    ROBERT 28/09/2017 03:49 pm

--- presentacion/Catalogo_proyectos_estatus.aspx.cs
+++ presentacion/Catalogo_proyectos_estatus.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using datos.Model;
+using negocio.Componentes;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +13,56 @@ namespace presentacion
 {
     public partial class catalogo_proyectos_estatus : System.Web.UI.Page
     {
+        private void ModalShow(string modalname)
+        {
+            System.Web.UI.ScriptManager.RegisterStartupScript(this, GetType(), Guid.NewGuid().ToString(),
+                             "ModalShow('" + modalname + "');", true);
+        }
+
+        private void ModalClose(string modalname)
+        {
+            System.Web.UI.ScriptManager.RegisterStartupScript(this, GetType(), Guid.NewGuid().ToString(),
+                             "ModalCloseGlobal('" + modalname + "');", true);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                CargarCatalogo();
+                //CargarListadoAyudasPadre();
+            }
         }
+
+        private DataTable GetProyectoEstatus()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                
+                AyudaCOM ayudas = new AyudaCOM();
+                dt = ayudas.SelectAll();
+            }
+            catch (Exception)
+            {
+                dt = new DataTable();
+            }
+            return dt;
+        }
+
+        private void CargarCatalogo()
+        {
+            try
+            {
+                DataTable dt = GetProyectoEstatus();
+                repeat_ayudas.DataSource = dt;
+                repeat_ayudas.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Toast.Error("Error al cargar catalogo: " + ex.Message, this);
+            }
+        }
+
     }
 }
--- presentacion/Catalogo_proyectos_estatus.aspx.designer.cs
+++ presentacion/Catalogo_proyectos_estatus.aspx.designer.cs
@@ -11,5 +11,104 @@ namespace presentacion {
     
     
     public partial class catalogo_proyectos_estatus {
+        
+        /// <summary>
+        /// Control lnknuevoproyectoestatus.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnknuevoproyectoestatus;
+        
+        /// <summary>
+        /// Control repeat_ayudas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repeat_ayudas;
+        
+        /// <summary>
+        /// Control UpdatePanel15.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel15;
+        
+        /// <summary>
+        /// Control txtestatus.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtestatus;
+        
+        /// <summary>
+        /// Control CheckBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
+        
+        /// <summary>
+        /// Control lnkcargando.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnkcargando;
+        
+        /// <summary>
+        /// Control lnkguardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnkguardar;
+        
+        /// <summary>
+        /// Control btneventgrid.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btneventgrid;
+        
+        /// <summary>
+        /// Control btneliminarpermiso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btneliminarpermiso;
+        
+        /// <summary>
+        /// Control hdfcommand.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdfcommand;
+        
+        /// <summary>
+        /// Control hdfid_proyectos_estatus.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdfid_proyectos_estatus;
     }
 }
