e05ea6a9a991259683397bad79f0b7721cbe947e
Isaac Humberto De la Rosa Mendez <bufon.3@hotmail.com>
2017-11-13 10:44:09 -0600

    HUMBERTO 13/11/2017

---
+++ presentacion/Pages/Compensaciones/compensaciones_solicitud.aspx.designer.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace presentacion.Pages.Compensaciones {
+    
+    
+    public partial class compensaciones_solicitud {
+        
+        /// <summary>
+        /// Control cbBonds_Types.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cbBonds_Types;
+    }
+}
