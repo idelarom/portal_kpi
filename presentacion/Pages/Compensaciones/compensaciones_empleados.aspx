f9b26d303779563c9250253e8316a8d1f8ada7ae
Roberto <isaaac_delarosa@migesa.com.mx>
2017-11-16 18:01:59 -0600

    ROBERT 16/11/2017 06:01 pm

---
+++ presentacion/Pages/Compensaciones/compensaciones_empleados.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/MP/Global.Master" AutoEventWireup="true" CodeBehind="compensaciones_empleados.aspx.cs" Inherits="presentacion.Pages.Compensaciones.compensaciones_empleados" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>
---
+++ presentacion/Pages/Compensaciones/compensaciones_empleados.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace presentacion.Pages.Compensaciones {
+    
+    
+    public partial class compensaciones_empleados {
+    }
+}
